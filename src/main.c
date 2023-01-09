220de9b7cc4c15c4c78c897aa9c0bfd3212f961d
root <root@DESKTOP-2TF5FH0.localdomain>
2023-01-09 17:28:48 +0100

    fixed ctrl signal

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: blaurent <blaurent@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 11:47:18 by blaurent          #+#    #+#             */
-/*   Updated: 2023/01/09 16:51:45 by blaurent         ###   ########.fr       */
+/*   Updated: 2023/01/08 18:14:35 by blaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,12 +51,7 @@ static	int	is_correct_input(char *input)
 		ft_putstr_fd("exit\n", 2);
 		exit(0);
 	}
-	if (is_heredoc(input))
-	{
-		ft_fprintf(2, "HEREDOC\n");
-		return (1);
-	}
-	else if (is_only_space(input) || input[0] == '\0' || is_invalid_syntax(input))
+	if (is_only_space(input) || input[0] == '\0' || is_invalid_syntax(input))
 		return (0);
 	add_history(input);
 	return (1);
@@ -104,6 +99,7 @@ int	main(int ac, char **av, char **envp)
 	while (ac && av)
 	{
 		signal(SIGINT, sigint_handler);
+		signal(SIGQUIT, SIG_IGN);
 		d.input = readline("=>");
 		if (is_correct_input(d.input))
 		{
