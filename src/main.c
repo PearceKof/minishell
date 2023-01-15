6e1b36ae617a4005e9eb17c924275ece6eda87aa
PearceKof <root@DESKTOP-0SM51MM.localdomain>
2023-01-15 17:59:52 +0100

    prompt error corr

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: blaurent <blaurent@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 11:47:18 by blaurent          #+#    #+#             */
-/*   Updated: 2023/01/15 17:56:08 by blaurent         ###   ########.fr       */
+/*   Updated: 2023/01/15 17:59:08 by blaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,6 +102,8 @@ void prompt(t_data *d)
 		free(prompt);
 		free(pwd);
 	}
+	else
+		d->input = readline(prompt);
 }
 
 int	main(int ac, char **av, char **envp)
